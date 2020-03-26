e04a7ebbae41d8f832653f09322f861dbf5a658d
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2020-03-26 12:11:47 +0100

    included commonTools and axios -- all still working --

--- app/renderer/App.js
+++ app/renderer/App.js
@@ -3,15 +3,29 @@
 */
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+import {dataDictionary2DataLabels} from "./commonTools";
+import {myURL} from "./definitions";
 
 export default class App extends Component {
   constructor(){
     super();
     this.state = {
-      targets: ["One","Two","Three"]
+      targets: []
     }
   }
-  
+
+  componentDidMount(){
+    const thePath = myURL+'/agile_science/-dataDictionary-';
+    axios(thePath).then((res) => {
+      const objLabel = dataDictionary2DataLabels(res.data);
+      const listLabels = objLabel.hierarchyList.concat(objLabel.dataList);
+      const targets = listLabels.map((docType,index)=> 
+                          {return listLabels[index][1];});
+      this.setState({targets: targets });
+    });
+  }
+    
   render() {
     const routeItems = this.state.targets.map(
       (item,idx)=>  <Route exact path={'/'+item} key={idx}>
