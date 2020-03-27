124205e3befa9bf2476e7d352c2d1c4e40fb2b85
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2020-03-27 10:36:00 +0100

    if local .json file does  not exist

--- app/renderer/credentials.js
+++ app/renderer/credentials.js
@@ -1,14 +1,26 @@
 function getCredentials(){
   try {                 // *** React-Electron version: has fs ***
     const fs = window.require('fs');
-    const config = JSON.parse( fs.readFileSync(process.env.HOME+'/.agileScience.json').toString() );
-    if (config['use']==='local') {
-      const local = {user: config.user, password: config.password, 
-        url: "http://127.0.0.1:5984",   database: config.database};
-      return local;
+    const path = process.env.HOME+'/.agileScience.json';
+    console.log('Read file: '+path);
+    if (fs.existsSync(path)) {
+      const config = JSON.parse( fs.readFileSync(path).toString() );
+      if (config['use']==='local') {
+        const local = {user: config.user, password: config.password, 
+          url: "http://127.0.0.1:5984",   database: config.database};
+        return local;
+      }
+      return config['remote'];
+    } else {
+      const remote = {
+        user: "6312f45e-b47e-43fb-b2c8-fd3ba43ea76d-bluemix",
+        password: "b0296a60a0f8879c09a3ceff62b67c175db9f8354fb84cfb44d7c2b3c961e86f",
+        url: "https://6312f45e-b47e-43fb-b2c8-fd3ba43ea76d-bluemix.cloudantnosqldb.appdomain.cloud",
+        database: "agile_science"
+        }
+      return remote;
     }
-    return config['remote'];
-    } catch(err) {    // *** React-DOM version: fails at fs ***
+  } catch(err) {    // *** React-DOM version: fails at fs ***
     const local = {user: 'admin', password: 'agile', 
                   url: "",   database: 'agile_science'}; //local url is produced via proxy in package.json
     return local;
