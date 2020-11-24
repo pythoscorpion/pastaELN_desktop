59d6c6e5b19a307d7c264531dff57551d9c51692
t.dueren <t.dueren@fz-juelich.de>
2020-11-24 14:37:43 +0100

    edit login

--- app/renderer/components/AboutPage.js
+++ app/renderer/components/AboutPage.js
@@ -6,17 +6,78 @@ export default class AboutPage extends Component {
   constructor() {
     super();
     this.state = {
-      credentials: ''
+      credentials:{
+        user: "",
+        password: "",
+        url: "",
+        database: ""
+      }, 
+      pwdBoxType: "password",
+      eyeType: "fa fa-eye"
     };
     this.submit = this.submit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputChangeUSR = this.handleInputChangeUSR.bind(this);
+    this.handleInputChangePW = this.handleInputChangePW.bind(this);
+    this.handleInputChangeURL = this.handleInputChangeURL.bind(this);
+    this.handleInputChangeDB = this.handleInputChangeDB.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
 
-  handleInputChange(event) {
+  handleInputChangeUSR(event) {
     this.setState({
-      credentials: event.target.value
+      credentials: {
+        user: event.target.value,
+        password: this.state.credentials.password,
+        url: this.state.credentials.url,
+        database: this.state.credentials.database
+      }
     });
   }
+  handleInputChangePW(event) {
+    this.setState({
+      credentials: {
+        user: this.state.credentials.user,
+        password: event.target.value,
+        url: this.state.credentials.url,
+        database: this.state.credentials.database
+      }
+    });
+  }
+  handleInputChangeURL(event) {
+    this.setState({
+      credentials: {
+        user: this.state.credentials.user,
+        password: this.state.credentials.password,
+        url: event.target.value,
+        database: this.state.credentials.database
+      }
+    });
+  }
+  handleInputChangeDB(event) {
+    this.setState({
+      credentials: {
+        user: this.state.credentials.user,
+        password: this.state.credentials.password,
+        url: this.state.credentials.url,
+        database: event.target.value
+      }
+    });
+  }
+
+  toggle() {
+    if(this.state.pwdBoxType==="password"){
+      this.setState({
+        pwdBoxType: "text",
+        eyeType: "fa fa-eye-slash"
+      });
+    } else if(this.state.pwdBoxType==="text"){
+      this.setState({
+        pwdBoxType: "password",
+        eyeType: "fa fa-eye"
+      });
+    }
+  }
+
   submit(){
     try {
       //try successful, use string; json-object can be ignored
@@ -44,11 +105,18 @@ export default class AboutPage extends Component {
 
   config() {
     return(<div>
-      <h3>Configuration</h3>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
+      <h3>Login</h3>
       <form>
         <label> Credentials: </label><br/>
-        <textarea rows='6' cols='50' value={this.state.credentials} onChange={this.handleInputChange}/><br/>
-        <input type='submit' value='Submit' onClick={this.submit}/>
+        <input type='text' placeholder='Username' value={this.state.credentials.user} onChange={this.handleInputChangeUSR}/><br/>
+        <input type={this.state.pwdBoxType} placeholder='Password' value={this.state.credentials.password} onChange={this.handleInputChangePW} id="pwdBox"/>
+        <button type='button' id='toggleButton' onClick={this.toggle}>
+          <i className={this.state.eyeType}></i>
+        </button><br/>
+        <input type='text' placeholder='database' value={this.state.credentials.database} onChange={this.handleInputChangeDB}/><br/>
+        <input type='text' placeholder='127.0.0.1' value={this.state.credentials.url} onChange={this.handleInputChangeURL}/><br/>
+        <input type='submit' value='login' onClick={this.submit}/>
       </form>
     </div>);
   }
