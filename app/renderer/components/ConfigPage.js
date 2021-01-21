3bc618fd9e1f3f64e6c562c67aa5b115d1d6d213
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2021-01-21 18:57:12 +0100

    Design configPage and create ModalOntology

--- app/renderer/components/ConfigPage.js
+++ app/renderer/components/ConfigPage.js
@@ -1,21 +1,18 @@
 /* Configuration of the software
   Elements:
-  - Login area
+  - Database configuration and ontology
   - Tasks:
     Test-Backend: testBackend
-    Edit Configuration: DBConfig
     Verify Database integrity: verifyDB
+    ...
   - About information
 */
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';                         // eslint-disable-line no-unused-vars
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // eslint-disable-line no-unused-vars
-import JSONInput from 'react-json-editor-ajrm';                   // eslint-disable-line no-unused-vars
-import locale    from 'react-json-editor-ajrm/locale/en';
-import { Button } from '@material-ui/core';
+import { Button, TextField, InputAdornment, IconButton, Input, FormControl, InputLabel } from '@material-ui/core';
+import { Visibility, VisibilityOff} from '@material-ui/icons';
 import {REACT_VERSION, executeCmd} from '../localInteraction';
-import Store from '../Store';
 import * as Actions from '../Actions';
+import ModalOntology from './ModalOntology';
 
 export default class ConfigPage extends Component {
   constructor() {
@@ -28,14 +25,12 @@ export default class ConfigPage extends Component {
         url: '',
         database: ''
       },
-      pwdBoxType: 'password',
-      eyeType: faEye,
+      showPassword: false,
       //other details
       ready: true,  //ready is for all task buttons
       btn_cfg_be_test:'#e0e0e0',
       btn_cfg_be_verifyDB:'#e0e0e0',
-      displayDBConfig: 'none',
-      ontologyObj: {},
+      displayOntology: 'none',
       testResult: ''
     };
   }
@@ -46,11 +41,6 @@ export default class ConfigPage extends Component {
       credentials: Object.assign(this.state.credentials,{[task]:event.target.value})
     });
   }
-  dbConfigChange=(event)=>{
-    if (event && 'error' in event && event.error===false) {
-      this.setState({ontologyObj:event.jsObject});
-    }
-  }
 
   // for all buttons
   pressedButton=(task)=>{  //sibling for pressedButton in Project.js: change both similarly
@@ -58,13 +48,6 @@ export default class ConfigPage extends Component {
     this.setState({ready: false});
     if (task.indexOf('_be_')>-1) {
       executeCmd(task,this.callback);
-    }
-    if (task=='loadJSON') {
-      this.setState({ontologyObj: Store.getOntology()});
-      Actions.comState('ok');
-    }
-    if (task=='saveJSON') {
-      Store.updateDocument(this.state.ontologyObj,false);
     }
     if (task=='btn_cfg_fe_credentials') {
       localStorage.setItem(task,JSON.stringify(this.state.credentials));
@@ -90,119 +73,147 @@ export default class ConfigPage extends Component {
   }
 
   //changes in visibility
-  toggleDBConfig=()=>{
-    if(this.state.displayDBConfig==='none') {
-      this.setState({displayDBConfig: 'block'});
+  toggleOntology=()=>{
+    if(this.state.displayOntology==='none') {
+      this.setState({displayOntology: 'block'});
     } else {
-      this.setState({displayDBConfig: 'none'});
+      this.setState({displayOntology: 'none'});
     }
   }
   togglePWD=()=>{
-    if(this.state.pwdBoxType==='password'){
-      this.setState({pwdBoxType: 'text', eyeType: faEyeSlash});
-    } else {
-      this.setState({pwdBoxType: 'password', eyeType: faEye});
-    }
+    this.setState({showPassword: !this.state.showPassword});
   }
 
   /* process data and create html-structure; all should return at least <div></div> */
-  showLogin() {
-    const {credentials, credVisibility, eyeType, pwdBoxType} = this.state;
-    return(
-      <div className="form-popup my-2" >
-        <form className="form-container">
-          <div className='row ml-1'>
-            <div>
-              <input type='text'       placeholder='Username' style={{visibility: credVisibility}} value={credentials.user}     onChange={e => this.loginChange(e,'user')} required size="30" /><br/>
-              <input type={pwdBoxType} placeholder='Password' style={{visibility: credVisibility}} value={credentials.password} onChange={e => this.loginChange(e,'password')} id='pwdBox' required  size="30" />
-              <button type='button'    id='toggleButton'      style={{visibility: credVisibility}} onClick={this.togglePWD}     tabIndex='-1'>
-                <FontAwesomeIcon       icon={eyeType}/>
-              </button><br/>
-              <input type='text'       placeholder='database'  value={credentials.database} onChange={e => this.loginChange(e,'database')} required size="30" /><br/>
-              <input type='text'       placeholder='127.0.0.1' value={credentials.url}      onChange={e => this.loginChange(e,'url')}      size="30" />
-            </div>
-            <div>
-              <Button type='submit' className='ml-2' variant="contained" onClick={() => this.pressedButton('btn_cfg_fe_credentials')} id='submitBtn'>Login</Button>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-
-  showDBConfig() {
+  showConfiguration() {
+    const {credentials, showPassword} = this.state;
     return(
       <div>
-        <Button className='btn-block' variant="contained"
-          onClick={this.toggleDBConfig}
-          disabled={!this.state.ready}>
-            Edit Database configuration
-        </Button>
-        <div className="modal" style={{display: this.state.displayDBConfig}}>
-          <div className="modal-content">
-            <div>
-              Remark: This will become easier and prettier.
-              <JSONInput
-                id          = 'jsonEditor'
-                placeholder = { this.state.ontologyObj }
-                onChange    = {e=> this.dbConfigChange(e)}
-                theme       = "light_mitsuketa_tribute"
-                width       = "800"
-                locale      = { locale }
-                style       = {{body:{fontSize:16}}}
-                colors      = {{keys:'#1E1E1E', colon:'#1E1E1E', default:'#386FA4'}}
-              />
-              <button onClick={() => this.pressedButton('loadJSON')} className='btn btn-secondary m-2' active={this.state.ready.toString()}>Load</button>
-              <button onClick={() => this.pressedButton('saveJSON')} className='btn btn-secondary m-2' active={this.state.ready.toString()}>Save</button>
-              <button onClick={() => this.toggleDBConfig()} className='close btn btn-secondary m-2' id='closeBtn'>Close</button>
+        <h1>Configuration</h1>
+        <div className='row mt-3'>
+          <div className='col-sm-6'>
+            Define to which server and which database on that server you want to connect. For local server, leave 127.0.0.1.
+          </div>
+          <div className='col-sm-6 px-0'>
+            <div className="form-popup my-2" >
+              <form className="form-container">
+                <div className='row ml-0'>
+                  <div className='col-sm-8'>
+                    <TextField type='text'       label='username' value={credentials.user}
+                      onChange={e=>this.loginChange(e,'user')}     required fullWidth/><br/>
+                    <FormControl fullWidth>
+                      <InputLabel htmlFor="standard-adornment-password">password</InputLabel>
+                      <Input fullWidth
+                        id="standard-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        value={credentials.password}
+                        onChange={e=>this.loginChange(e,'password')}
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton aria-label="toggle password visibility" onClick={this.togglePWD}>
+                              {showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                          </InputAdornment>}
+                      />
+                    </FormControl>
+                    <TextField type='text'       label='database'  value={credentials.database}
+                      onChange={e=>this.loginChange(e,'database')} required fullWidth /><br/>
+                    <TextField type='text'       label='127.0.0.1' value={credentials.url}
+                      onChange={e=>this.loginChange(e,'url')}      fullWidth />
+                  </div>
+                  <div className='col-sm-4 row'>
+                    <Button type='submit' className='btn-block' variant="contained"
+                      onClick={() => this.pressedButton('btn_cfg_fe_credentials')} id='submitBtn'>
+                        Login
+                    </Button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </div>);
+        <div className='row mt-3'>
+          <div className='col-sm-6'>
+            Define what data types (projects, samples, ...) with which meta data (name, comments, ...) you want to store.
+          </div>
+          <div className='col-sm-6'>
+            <Button className='btn-block' variant="contained"
+              onClick={this.toggleOntology}
+              disabled={!this.state.ready}>
+                Edit Ontology
+            </Button>
+            <ModalOntology display={this.state.displayOntology} callback={this.toggleOntology} />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   showTasks(){
     return(
       <div>
         <h1>Tasks</h1>
-        <div style={{width:350}}>
-          <Button style={{backgroundColor:this.state.btn_cfg_be_test}}
-            onClick={() => this.pressedButton('btn_cfg_be_test')}
-            className='btn-block' variant="contained"
-            disabled={!this.state.ready}>
-            Test backend / Create views
-          </Button>
-
-          <Button style={{backgroundColor:this.state.btn_cfg_be_verifyDB}}
-            onClick={() => this.pressedButton('btn_cfg_be_verifyDB')}
-            className='my-3 btn-block' variant="contained"
-            disabled={!this.state.ready}>
-            Verify database integrity
-          </Button>
-
-          <div className='row p-0'>
-            <div className='col-sm-6'>
-              <Button onClick={() => this.pressedButton('btn_cfg_be_saveBackup')}
-                className='btn-block' variant="contained"
-                disabled={!this.state.ready}>
-                Save backup
-              </Button>
-            </div>
-            <div className='col-sm-6'>
-              <Button onClick={() => this.pressedButton('btn_cfg_be_loadBackup')}
-                className='btn-block' variant="contained"
-                disabled={!this.state.ready}>
-                Load backup
-              </Button>
-            </div>
+        <div className='row'>
+          <div className='col-sm-6'>
+            Troubleshooting:<br />
+            Test whether python backend is operational and the views (source of tables for projects,...) exist.<br />
+            Test database logic: e.g. revisions make sense, QR codes exist for samples,...
           </div>
-
-          <Button
-            onClick={() => this.pressedButton('btn_cfg_be_updateJamDB')}
-            className='mt-3 btn-block' variant="contained"
-            disabled={!this.state.ready}>
-            Update software
-          </Button>
+          <div className='col-sm-6'>
+            <Button style={{backgroundColor:this.state.btn_cfg_be_test}}
+              onClick={() => this.pressedButton('btn_cfg_be_test')}
+              className='btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Test backend / Create views
+            </Button>
+            <Button style={{backgroundColor:this.state.btn_cfg_be_verifyDB}}
+              onClick={() => this.pressedButton('btn_cfg_be_verifyDB')}
+              className='mt-2 btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Verify database integrity
+            </Button>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col-sm-6'>
+            Backup files are zip-files which include all the meta data. Unzip and open the resulting json-files with web-browser.
+          </div>
+          <div className='col-sm-6'>
+            <Button onClick={() => this.pressedButton('btn_cfg_be_saveBackup')}
+              className='btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Save backup
+            </Button>
+            <Button onClick={() => this.pressedButton('btn_cfg_be_loadBackup')}
+              className='mt-2 btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Load backup
+            </Button>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col-sm-6'>
+            Download update to backend and frontend from jugit.fz-juelich.de server. After update, restart software with Ctrl-R.
+          </div>
+          <div className='col-sm-6'>
+            <Button
+              onClick={() => this.pressedButton('btn_cfg_be_updateJamDB')}
+              className='btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Update software
+            </Button>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col-sm-6'>
+            Log of backend activity.<br />
+            <Button onClick={() => {this.setState({testResult:''});}} className='mt-2' variant="contained">
+              Clear log
+            </Button>
+          </div>
+          <div className='col-sm-6'>
+            <TextField multiline rows={8} fullWidth value={this.state.testResult} variant="outlined" InputProps={{readOnly: true}}/>
+          </div>
         </div>
       </div>
     );
@@ -225,7 +236,7 @@ export default class ConfigPage extends Component {
           git for software development.
         </p>
         <p>
-          <a target="_blank"  href=' https://youtu.be/9nVMqMs1Wvw'>A teaser youtube video...</a> <br />
+          <a target="_blank"  href='https://youtu.be/9nVMqMs1Wvw'>A teaser youtube video...</a> <br />
           <a target="_blank"  href='https://jugit.fz-juelich.de/s.brinckmann/jamdb-python/-/wikis/home'>For more information...</a>
         </p>
       </div>
@@ -237,26 +248,14 @@ export default class ConfigPage extends Component {
     return (
       <div className='container px-4 pt-2'>
         <div className='border my-4 p-3'>
-          <h1>{REACT_VERSION==='Electron' ? 'Remote server details' : 'Login'} </h1>
-          {this.showLogin()}
+          {this.showConfiguration()}
         </div>
-        {this.showDBConfig()}
 
         { (REACT_VERSION==='Electron') &&    // *** React-Electron version
           <div className='border my-4 p-3'>
             {this.showTasks()}
           </div>
         }
-
-        <div className='border my-4 p-3'>
-          <h1>Log</h1>
-          <div style={{width:350}}>
-            <Button onClick={() => {this.setState({testResult:''});}} className='my-2 btn-block' variant="contained">
-              Clean log
-            </Button>
-            <textarea rows="8" cols="50" value={this.state.testResult} readOnly className='my-2'></textarea>
-          </div>
-        </div>
 
         <div className='border my-4 p-3'>
           {this.showAbout()}
---
+++ app/renderer/components/ModalOntology.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';                         // eslint-disable-line no-unused-vars
+import JSONInput from 'react-json-editor-ajrm';                   // eslint-disable-line no-unused-vars
+import locale    from 'react-json-editor-ajrm/locale/en';
+import { Button } from '@material-ui/core';
+import Store from '../Store';
+
+export default class ModalOntology extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ontologyObj: {}
+     };
+  }
+
+  dbConfigChange=(event)=>{
+    if (event && 'error' in event && event.error===false) {
+      this.setState({ontologyObj:event.jsObject});
+    }
+  }
+
+  pressedLoadBtn=()=>{
+    this.setState({ontologyObj: Store.getOntology()});
+  }
+  pressedSaveBtn=()=>{
+    Store.updateDocument(this.state.ontologyObj,false);
+    this.props.callback();
+  }
+
+  render(){
+    return (
+      <div className="modal" style={{display: this.props.display}}>
+        <div className="modal-content">
+          <h1>Remark: This will become easier and prettier.</h1>
+          <div>
+            <JSONInput
+              id          = 'jsonEditor'
+              placeholder = { this.state.ontologyObj }
+              onChange    = {e=> this.dbConfigChange(e)}
+              theme       = "light_mitsuketa_tribute"
+              width       = "800"
+              locale      = { locale }
+              style       = {{body:{fontSize:16}}}
+              colors      = {{keys:'#1E1E1E', colon:'#1E1E1E', default:'#386FA4'}}
+            />
+            <Button onClick={() => this.pressedLoadBtn()}
+              variant="contained" size="large"
+              className='m-3'>
+                Load
+            </Button>
+            <Button onClick={() => this.pressedSaveBtn()}
+              variant="contained" size="large"
+              className='m-3'>
+                Save
+            </Button>
+            <Button onClick={() => this.props.callback()}
+              variant="contained" size="large"
+              className='float-right m-3'
+              id='closeBtn'>
+                Cancel
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+/*
+<Button
+              onClick={() => this.pressedButton('btn_cfg_be_updateJamDB')}
+              className='btn-block' variant="contained"
+              disabled={!this.state.ready}>
+              Update software
+            </Button>
+            */
